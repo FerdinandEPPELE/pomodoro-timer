1b9465160d9660d9f9367078330bce017184b9d1
Your Name <you@example.com>
2023-09-10 13:42:19 +0200

    Ajout du bouton "Start", et du timer.

---
+++ js/timer.js
@@ -0,0 +1,66 @@
+// Déclaration des variables
+let timer;
+let minutes = 20;
+let seconds = 0;
+
+// Récupération des boutons
+const startButton = document.querySelector('button:nth-of-type(1)');
+const pauseButton = document.querySelector('button:nth-of-type(2)');
+const stopButton = document.querySelector('button:nth-of-type(3)');
+
+// Fonction pour mettre à jour l'affichage du chronomètre
+function updateTimerDisplay() {
+  const timerDisplay = document.querySelector('#timer-display');
+  timerDisplay.textContent = `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
+}
+
+// Fonction pour démarrer le chronomètre
+function startTimer() {
+  timer = setInterval(function () {
+    if (seconds > 0) {
+      seconds--;
+    } else {
+      if (minutes > 0) {
+        minutes--;
+        seconds = 59;
+      } else {
+        clearInterval(timer);
+        alert('Chronomètre terminé !');
+      }
+    }
+    updateTimerDisplay();
+  }, 1000);
+
+  // Désactiver le bouton "Start" et activer le bouton "Pause"
+  startButton.setAttribute('disabled', 'true');
+  pauseButton.removeAttribute('disabled');
+}
+
+// Fonction pour mettre en pause le chronomètre
+function pauseTimer() {
+  clearInterval(timer);
+
+  // Activer le bouton "Start" et désactiver le bouton "Pause"
+  startButton.removeAttribute('disabled');
+  pauseButton.setAttribute('disabled', 'true');
+}
+
+// Fonction pour réinitialiser le chronomètre
+function stopTimer() {
+  clearInterval(timer);
+  minutes = 20;
+  seconds = 0;
+  updateTimerDisplay();
+
+  // Activer le bouton "Start" et désactiver le bouton "Pause"
+  startButton.removeAttribute('disabled');
+  pauseButton.setAttribute('disabled', 'true');
+}
+
+// Écouteurs d'événements pour les boutons
+startButton.addEventListener('click', startTimer);
+pauseButton.addEventListener('click', pauseTimer);
+stopButton.addEventListener('click', stopTimer);
+
+// Appel initial pour afficher le temps initial
+updateTimerDisplay();
