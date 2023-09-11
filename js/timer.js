77ff4203f234f2a5bfe315ee7864dd034dd31d2d
FerdinandEPPELE <123150329+FerdinandEPPELE@users.noreply.github.com>
2023-09-11 09:31:41 +0200

    Update timer.js, ajout du libelle pause et travail.

--- js/timer.js
+++ js/timer.js
@@ -1,12 +1,16 @@
 // Déclaration des variables
 let timer;
-let minutes = 20;
-let seconds = 0;
+let minutes = 0;
+let seconds = 2;
 
 // Récupération des boutons
 const startButton = document.querySelector('button:nth-of-type(1)');
 const pauseButton = document.querySelector('button:nth-of-type(2)');
 const stopButton = document.querySelector('button:nth-of-type(3)');
+const libelleTravail = document.getElementById("travail");
+const libellePause = document.getElementById("pause");
+
+libellePause.style.visibility = 'hidden'
 
 // Fonction pour mettre à jour l'affichage du chronomètre
 function updateTimerDisplay() {
@@ -25,7 +29,7 @@ function startTimer() {
         seconds = 59;
       } else {
         clearInterval(timer);
-        alert('Chronomètre terminé !');
+        switchTimer();
       }
     }
     updateTimerDisplay();
@@ -34,6 +38,7 @@ function startTimer() {
   // Désactiver le bouton "Start" et activer le bouton "Pause"
   startButton.setAttribute('disabled', 'true');
   pauseButton.removeAttribute('disabled');
+
 }
 
 // Fonction pour mettre en pause le chronomètre
@@ -48,13 +53,32 @@ function pauseTimer() {
 // Fonction pour réinitialiser le chronomètre
 function stopTimer() {
   clearInterval(timer);
-  minutes = 20;
-  seconds = 0;
+  minutes = 0;
+  seconds = 5;
   updateTimerDisplay();
 
   // Activer le bouton "Start" et désactiver le bouton "Pause"
   startButton.removeAttribute('disabled');
   pauseButton.setAttribute('disabled', 'true');
+}
+
+//Fonction pour switch en le temps de pause et le temps de travail.
+function switchTimer() {
+    if(minutes == 0 && seconds == 0 && libellePause.style.visibility == 'hidden') {
+        libelleTravail.style.visibility = 'hidden'
+        libellePause.style.visibility = 'visible'
+        minutes = 0;
+        seconds = 2;
+        startButton.removeAttribute('disabled');
+    } else {
+        if(minutes == 0 && seconds == 0 && libelleTravail.style.visibility == 'hidden') {
+            libelleTravail.style.visibility = 'visible'
+            libellePause.style.visibility = 'hidden'
+            minutes = 0;
+            seconds = 2;
+            startButton.removeAttribute('disabled');
+        }
+    }
 }
 
 // Écouteurs d'événements pour les boutons
